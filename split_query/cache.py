c8cebc065ac7dfbc19e93bdcd883dfa7e31d1f49
Simon Bowly <simon.bowly@gmail.com>
2017-12-20 10:22:48 +1100

    Update MinimalCache to create a query plan before assembling any data.

--- split_query/cache.py
+++ split_query/cache.py
@@ -1,6 +1,7 @@
 
 from builtins import super
 
+from contextlib import closing
 import json
 import logging
 import os
@@ -26,7 +27,8 @@ def simplify(expression):
 class MinimalCache(object):
     ''' Cache implementation that uses cached data as much as possible
     (minimal download policy). Uses a simple iterative algorithm, subtracting
-    each cached dataset in sequence from the required data. '''
+    each cached dataset in sequence from the required data. The :cache object
+    must implement the dictionary interface (getitem/setitem/keys). '''
 
     def __init__(self, remote, cache):
         self.remote = remote
@@ -35,13 +37,13 @@ class MinimalCache(object):
     def get(self, expression):
         ''' Sequentially eliminates parts of the input query with overlapping
         data from the cache. Queries the remote for any missing entries. '''
-        parts = []
-        for cached_query, cached_data in self.cache.items():
+        plan = []
+        for cached_query in self.cache.keys():
             # If the cache element overlaps the current expression, add it
             # to the partial data and replace expression with remainder.
             intersection = simplify(And([expression, cached_query]))
             if intersection is not False:
-                parts.append(query_df(cached_data, expression))
+                plan.append((cached_query, expression))
                 expression = simplify(And([expression, Not(cached_query)]))
             # If there is no remainder, we can stop.
             if expression is False:
@@ -50,38 +52,67 @@ class MinimalCache(object):
             # No break: there is missing data to be retrieved from remote.
             actual, data = self.remote.get(expression)
             self.cache[actual] = data
-            parts.append(query_df(data, expression))
-        # Assemble final result.
-        return pd.concat(parts)
+            plan.append((actual, expression))
+            expression = simplify(And([expression, Not(actual)]))
+            assert expression is False          # TODO query not satisfied.
+        # Assemble result from the plan.
+        return pd.concat(
+            query_df(self.cache[cached_query], filter_query)
+            for cached_query, filter_query in plan)
 
 
 class PersistentDict(object):
     ''' dict-like interface which keeps a contents file using shelve and
-    writes data using hdf5. '''
+    writes data using hdf5. Expression keys are serialised to JSON (shelf data
+    must be binary-encodable).
+    TODO keep an in-memory copy of contents for faster __getitem__. '''
 
     def __init__(self, location):
         self.location = location
         self.contents_file = os.path.join(self.location, 'contents')
+        self.local_contents = dict()
 
-    def items(self):
+    @staticmethod
+    def decode_shelf(shelf):
+        return {
+            json.loads(key, object_hook=object_hook): data_id
+            for key, data_id in shelf.items()}
+
+    def _update_local_contents(self):
+        ''' Reads the contents file to update the local copy. Expression keys
+        are decoded. '''
         if not os.path.exists(self.location):
             os.makedirs(self.location)
-        shelf = shelve.open(self.contents_file)
-        for key, data_id in shelf.items():
-            expr = json.loads(key, object_hook=object_hook)
-            value = pd.read_hdf(os.path.join(self.location, data_id))
-            yield expr, value
-        shelf.close()
+        with closing(shelve.open(self.contents_file)) as shelf:
+            self.local_contents = self.decode_shelf(shelf)
 
-    def __setitem__(self, expr, data):
-        if not os.path.exists(self.location):
-            os.makedirs(self.location)
-        shelf = shelve.open(self.contents_file)
-        key = json.dumps(expr, default=default)
+    def keys(self):
+        ''' Update local_contents and return expression keys. '''
+        self._update_local_contents()
+        return self.local_contents.keys()
+
+    def __getitem__(self, expression):
+        ''' Return data corresponding to the given :expression. If :expression
+        is not in the local copy of contents, update local copy before
+        attempting to get the data identifier. '''
+        if expression not in self.local_contents:
+            self._update_local_contents()
+        data_id = self.local_contents[expression]
+        return pd.read_hdf(os.path.join(self.location, data_id))
+
+    def __setitem__(self, expression, data):
+        ''' Write a new expression key to contents shelf with a unique data
+        identifier, write data to file given by the identifier. Updates
+        local_contents after adding the new key. Data is written first, so if
+        there are errors in data writing, the contents will not be updated. '''
         data_id = str(uuid.uuid4())
-        shelf[key] = data_id
         data.to_hdf(os.path.join(self.location, data_id), key='main', complevel=3)
-        shelf.close()
+        if not os.path.exists(self.location):
+            os.makedirs(self.location)
+        with closing(shelve.open(self.contents_file)) as shelf:
+            key = json.dumps(expression, default=default)
+            shelf[key] = data_id
+            self.local_contents = self.decode_shelf(shelf)
 
 
 def minimal_cache_inmemory(remote):
